f740e2c529de780259ba93332a3879c83a83b271
kronax61 <m.bilal.alpaslan@gmail.com>
2021-03-31 13:29:36 +0300

    init page

--- src/kariyerinit.js
+++ src/kariyerinit.js
@@ -1,42 +1,42 @@
 export const data =[
     {
         "name":"backend",
-        "baslik":"backend yazı",
-        "roadmap":["python","oop","moduls"],
+        "baslik":"Backend genellikle bir sunucu, bir uygulama ve bir veri tabanından oluşur. Bir web sitesine girerek işlem yaptığınızda frontend ile etkileşime girmiş olursunuz. Siz bilgilerinizi web sitesine girdiğinizde, backend bu bilgiyi alır ve bir sunucu üzerinde kurulmuş olan veri tabanına depolar",
+        "roadmap":["programlamaya giriş","nesne tabanlı programlama","veri yapıları","web protokolleri","Rest api"],
     },
     {
         "name":"frontend",
-        "baslik":"frontend yazı",
-        "roadmap":["html","css","js"],
+        "baslik":"frontend, web'in görüp etkileşime girebildiğiniz kısımlarına verilen addır. Frontend genellikle web tasarımı ve web'in ön yüzünün geliştirilmesini kapsar. Web tasarımı dendiğinde; Adobe XD ve Photoshop gibi tasarım programlarını kullanabilen, aynı zamanda da HTML, CSS, JavaScript ile kod yazabilen kişilerden bahsedilir. Bir web sitesini kullanırken gördüğünüz her şey; HTML, CSS ve JavaScript'in, kullandığınız web tarayıcı tarafından kontrol edilmesidir. Bunun içinde de fontlar, açılır menüler, geçişler, sliderlar, iletişim formları vb. tasarımsal işler yer alır.",
+        "roadmap":["html","css","javascript","javascript framework(react,angular,vue)"],
     },
     {
         "name":"mobil",
-        "baslik":"mobil yazı",
-        "roadmap":["flutter","rest api","websocket"],
+        "baslik":"Gelişen teknolojiyle küçülerek cebimizde taşınabilen hale gelen bu akıllı cihazlarda çalışan yazılımlar",
+        "roadmap":["flutter","sqflite","firebase","websocket","Rest api"],
     },
     {
         "name":"devops",
-        "baslik":"devops yazı",
-        "roadmap":["docker","linux","k8s"],
+        "baslik":"DevOps , Bilgi Teknolojileri departmanı içerisinde bulunan iki temel birimi (Developers and Operations) Geliştiriciler(Yazılım Geliştiriciler, Yazılım Testçileri, vb.), Operasyon (Sistem Mimari ve Altyapı Ekipleri,Güvenlik ve Ağ ekipleri vb.) bir arada etkili bir iletişim içerisinde beraber çalışmalarıdır.",
+        "roadmap":["linux","load balancer/nginx","docker","kubernates","jenkins"],
     },
     {
         "name":"ai",
-        "baslik":"ai yazı",
-        "roadmap":["istatistik","lineer cebir","python"],
+        "baslik":"Bir bilgisayarın faaliyetleri zeki canlılara benzer şekilde yerine getirmesi için yapılan yazılımdır",
+        "roadmap":["istatistik","lineer cebir","python/r","machine learning","Deep learning"],
     },
     {
         "name":"robotik",
-        "baslik":"robotik yazı",
-        "roadmap":["ros","python","raspberry pi"],
+        "baslik":"birçok mühendislik dallının ortak çalışma alanıdır. Yazılım aracılığıyla yönetilen ve yararlı bir amaç için iş ve değer üreten karmaşık makinelerin üretimidir.",
+        "roadmap":["python","raspberry pi","ros"],
     },
     {
         "name":"gömülü",
-        "baslik":"gömülü yazı",
+        "baslik":"Gömülü sistem, bilgisayarın kendisini kontrol eden cihaz tarafından içerildiği özel amaçlı bir sistemdir. Genel maksatlı, örneğin kişisel bilgisayar gibi bir bilgisayardan farklı olarak, gömülü bir sistem kendisi için önceden özel olarak tanımlanmış görevleri yerine getirir. ",
         "roadmap":["c","arduino/pic","elektronik"],
     },
     {
         "name":"siber",
-        "baslik":"siber yazı",
-        "roadmap":["linux","bilgisayar ağları","kriptoloji"],
+        "baslik":"bilgisayarları, sunucuları, mobil cihazları, elektronik sistemleri, ağları ve verileri kötü amaçlı saldırılardan koruma uygulamasıdır. Bilgi teknolojisi güvenliği veya elektronik bilgi güvenliği olarak da bilinir.",
+        "roadmap":["linux","bilgisayar ağları","kriptoloji","işletim sistemleri"],
     },
 ]
